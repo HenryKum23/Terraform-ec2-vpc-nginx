f5477e0ce13a49ad1fe69bc34762202b8c74d2d1
Henry Kumah <142521180+HenryKum23@users.noreply.github.com>
2024-06-14 13:49:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,15 +34,6 @@ module "ec2_instances" {
   }
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
-
-#   tags = {
-#     Name = "main"
-#   }
-# }
-
 resource "aws_default_vpc" "default" {
 
 }
